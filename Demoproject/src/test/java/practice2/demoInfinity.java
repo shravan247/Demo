cbc9fafe413d20cd30e1c69bc44185841be93485
shravan247 <shravansshetty701@gmail.com>
2024-03-23 08:45:32 +0530

    3rd pushhh

---
+++ Demoproject/src/test/java/practice2/demoInfinity.java
@@ -0,0 +1,9 @@
+package practice2;
+
+public class demoInfinity {
+
+	public static void main(String[] args) {
+System.out.println("Hey this is Shravan Shetty from Qspiders OAR");
+	}
+
+}
--- Demoproject/src/test/java/practice2/demoo.java
+++ Demoproject/src/test/java/practice2/demoo.java
@@ -2,6 +2,6 @@ package practice2;
 
 public class demoo {
 	public static void main(String[] args) {
-		System.out.println("hiii im seconf demo");
+		System.out.println("hiii im second demo");
 	}
 }
